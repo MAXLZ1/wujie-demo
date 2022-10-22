26e0c54cf515f17cba769eff1a90ce6c8d12f588
MAXLZ <2251683416@qq.com>
2022-10-22 19:11:20 +0800

    feat: init project

--- packages/react-child/src/App.tsx
+++ packages/react-child/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WujieReact from 'wujie-react';
 
@@ -7,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React子应用</h1>
         <WujieReact name="vue2" url="//localhost:9001" />
       </header>
     </div>
